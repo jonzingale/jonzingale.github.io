cb50896a27a79bdcdafe9dcd6a9406c1cab3222e
Jon Zingale <jonzingale@gmail.com>
2019-05-17 18:05:45 -0600

    better css for groups

--- galois/helpers.js
+++ galois/helpers.js
@@ -1,7 +1,3 @@
-var myScale = d3.scaleLinear()
-  .domain([0, 10])
-  .range([0, 600]);
-
 function pp(data) {
   console.log(JSON.stringify(data))
 }
@@ -9,7 +5,24 @@ function pp(data) {
 function mod(a,b){ return(((a % b) + b) % b) }
 function div(a,b){ return Math.floor(a/b) }
 
+var gcd = function(a, b) {
+  if ( ! b) { return a }
+  return gcd(b, a % b)
+};
+
 function range(n, ary=[]) {
   for (i=0; i < n; i++) { ary.push(i) }
   return ary
+}
+
+function primitives(p, tot=[]) {
+  var phi = range(p).filter(y => gcd(y,p) == 1)
+
+  phi.forEach(function(i) {
+    phi.forEach(function(j){
+      tot.push(mod(i*j, p)) 
+    })
+  })
+
+  return tot
 }
