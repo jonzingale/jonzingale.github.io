ac8ea4a35065ee02b719ab5f1d88224cb3745a8d
Jon Zingale <jonzingale@gmail.com>
2019-05-17 23:54:32 -0600

    working multiplicative groups with radio buttons

--- galois/helpers.js
+++ galois/helpers.js
@@ -26,3 +26,41 @@ function primitives(p, tot=[]) {
 
   return tot
 }
+
+// radios
+const controlbox_width = 600,
+     controlbox_height = 100,
+     n_grid_x = 7,
+     n_grid_y = 9
+
+var controls =
+  d3.selectAll(".group-controls").append("svg")
+    .attr("class", "group_widgets")
+
+var g = widget.grid(controlbox_width,
+                    controlbox_height,
+                    n_grid_x,
+                    n_grid_y);
+
+var radioblock1 = g.block({x0:10,y0:8,width:6,height:0});
+
+var choices = [5,7,8,11,12,13,16,17,29,23,31,32,61,101]
+var c1 = {id:"ns", name:"n", choices: choices, value:1};
+
+var radios1 = [
+  widget.radio(c1).size(radioblock1.w()).label("bottom")
+  .shape("rect").fontSize(16).orientation("horizontal")
+  .update(resetsystem)
+]
+
+var rad1 = controls.selectAll(".radio .r1")
+  .data(radios1).enter().append(widget.radioElement)
+  .attr("transform",function(d,i){ return "translate(620,11)" });  
+
+function resetsystem(){
+  gSize = choices[c1.value]
+  d3.select('.grid-container').selectAll("*").remove();
+  render()
+}
+
+render()
