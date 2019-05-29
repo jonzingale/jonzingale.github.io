728b90da739079bb5ca67b6a94e381cf426ab3bb
Jon Zingale <jonzingale@gmail.com>
2019-05-28 21:18:32 -0600

    div to match mod

--- js/helpers.js
+++ js/helpers.js
@@ -9,3 +9,7 @@ function normalize(array, total=0) {
 function pp(data) {
   console.log(JSON.stringify(data))
 }
+
+function mod(a,b){return(((a % b) + b) % b)}
+
+function div(a,b){return(Math.floor(a/b))}
