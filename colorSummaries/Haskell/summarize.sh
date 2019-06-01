e92dc3c043d6fa165a99b4a427e7b8176237c02e
Jon Zingale <jonzingale@gmail.com>
2019-06-01 17:22:31 -0600

    script for summaries

--- colorSummaries/Haskell/summarize.sh
+++ colorSummaries/Haskell/summarize.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+# https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/profiling.html
 
-ghc -O2 --make VectorColorSummarizer.hs -threaded -rtsopts
-time ./VectorColorSummarizer +RTS -N8
-rm VectorColorSummarizer.hi VectorColorSummarizer.o VectorColorSummarizer
+# ghc -O2 --make VectorColorSummarizer.hs -rtsopts -fforce-recomp -prof -fprof-auto -fprof-cafs
+# echo "Running Summarizer"
+# time ./VectorColorSummarizer +RTS -sstderr -p
+
+# rm VectorColorSummarizer.hi VectorColorSummarizer.o
+# rm PrismaJSON.hi PrismaJSON.o PrismaMatcher.hi PrismaMatcher.o
+# rm VectorColorSummarizer
+
+ghc -O2 --make VectorColorSummarizer.hs
+echo "Running Summarizer"
+file="$1" # "/Users/Jon/Desktop/californiaPoppy.jpg" 
+time ./VectorColorSummarizer $file
+rm VectorColorSummarizer.hi VectorColorSummarizer.o
+rm PrismaJSON.hi PrismaJSON.o PrismaMatcher.hi PrismaMatcher.o
+rm VectorColorSummarizer
+
+
+# EFFECTIVELY TO RUN THIS SCRIPT:
+# ./summarize.sh "/Users/Jon/Desktop/californiaPoppy.jpg" 
