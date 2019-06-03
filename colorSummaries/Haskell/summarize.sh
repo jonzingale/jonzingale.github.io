f9339e9272578de5f84a25a4f5ded0fea9745ea2
Jon Zingale <jonzingale@gmail.com>
2019-06-02 22:59:50 -0700

    towards automation

--- colorSummaries/Haskell/summarize.sh
+++ colorSummaries/Haskell/summarize.sh
@@ -21,11 +21,12 @@ time ./ColorSummarizer $clusters $file
 # rm PrismaJSON.hi PrismaJSON.o PrismaMatcher.hi PrismaMatcher.o
 
 # seems to work, needs a kill to clean the process.
-# cd ./../..
-# python -m http.server &
-open "http://localhost:8000/colorSummaries/Haskell/prisma.html"
-# & fg
-
+cd ./../..
+python -m http.server &
+open "http://localhost:8000/colorSummaries/Haskell/prisma.html" &
+# open -na "Google Chrome" --args --new-window "http://localhost:8000/colorSummaries/Haskell/prisma.html" &
+sleep 15
+kill $(ps aux | grep '[p]ython -m http.server' | awk '{print $2}')
 # rm ColorSummarizer
 
 
