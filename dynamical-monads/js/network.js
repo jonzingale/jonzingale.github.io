496af0dd45d54e157391800bbfb4749ee4f8e5d7
Jon Zingale <jonzingale@gmail.com>
2020-03-21 23:02:37 -0600

    better spacing

--- dynamical-monads/js/network.js
+++ dynamical-monads/js/network.js
@@ -6,7 +6,7 @@ function network() {
   var simulation = d3.forceSimulation()
       .force("link", d3.forceLink().id(function(d) { return d.id; }))
       .force("charge", d3.forceManyBody().strength(
-        function(d){ return -d.degree * 3 } )
+        function(d){ return -d.degree * 4 } )
       )
       .force("center", d3.forceCenter(width / 2, height / 2));
 
