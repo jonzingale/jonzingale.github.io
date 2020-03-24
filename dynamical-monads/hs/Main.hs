f13715964e07cce80550efe1716d3f0ab3bb3bb3
Jon Zingale <jonzingale@gmail.com>
2020-03-24 12:43:42 -0600

    moved haskell files to hs

---
+++ dynamical-monads/hs/Main.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module DynamicalApi where
+
+import qualified Data.ByteString.Lazy as BL
+import GHC.Generics
+import Data.Text (Text)
+import Data.Csv
+
+import DynamicalExamples (g1, g2, g3, g4)
+import CsvRecord
+import Dynamical
+import Product
+
+{--
+TODO:
+* avoid name space crashes
+--}
+
+mRecords = do -- not yet used on frontend, incomplete specification
+  let mg = eval.unit $ g1 + g4
+  let recs = [MRecord s t (etaName (E s t)) | (E s t) <- mg]
+  let m_a = encodeDefaultOrderedByName recs
+  BL.writeFile "./data/m_a_test.csv" m_a
+
+mmRecords = do -- not yet used on frontend, incomplete specification
+  let mmg = eval.unit.eval.unit $ g1 + g4
+  let recs = [MMRecord s t (muName (E s t)) | (E s t) <- mmg]
+  let m_a = encodeDefaultOrderedByName recs
+  BL.writeFile "./data/m_m_a_test.csv" m_a
+
+-- Perhaps instead of the above make a M1_Record which includes the unit name
+-- and make an M2_record which includes both unit name and mu name.
+
+main = do
+  -- let g = g1 + g2 -- edge^2, 3-cycle with leg
+  -- let g = g1 + g4 -- 2-cycle, edge^2
+  let g = [E "0" "0", E "1" "0", E "2" "1"] -- tail
+  let a = encodeDefaultOrderedByName g
+  let m_a = encodeDefaultOrderedByName.eval.unit $ g
+  let m_m_a = encodeDefaultOrderedByName $ (eval.unit).(eval.unit) $ g
+  BL.writeFile "../data/dyn.csv" a
+  BL.writeFile "../data/m_a_dyn.csv" m_a
+  BL.writeFile "../data/m_m_a_dyn.csv" m_m_a
---
+++ dynamical-monads/hs/Product.hs
@@ -0,0 +1,31 @@
+module Product where
+import Prelude hiding (Monad, (>>=))
+import Dynamical
+
+data Product s = TT { pr1::s, pr2 ::s } deriving (Show)
+
+instance Functor Product where
+  fmap f (TT p q) = TT (f p) (f q)
+
+instance Applicative Product where
+  pure x = TT x x
+  (TT f g) <*> (TT x y) = TT (f x) (g y)
+
+class Functor m => Monad m where
+  unit :: a -> m a
+  join :: m (m a) -> m a
+  (>>=) :: m a -> (a -> m b) -> m b 
+
+instance Monad Product where
+  unit x = pure x
+  join (TT x y) = TT (pr1 x) (pr2 y)
+  x >>= f = join (f <$> x)
+
+unitGraph :: Product (Graph String)
+unitGraph = unit graph
+joinGraph = join $ unit unitGraph
+appendRight = (unit graph) >>= (\g -> TT graph g)
+
+-- USAGE: eval appendRight
+eval :: Product (Graph [s]) -> Graph [s]
+eval pg = pr1 pg * pr2 pg
