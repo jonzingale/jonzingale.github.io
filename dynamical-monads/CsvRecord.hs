4c63e2ff3a395346abf28dbd99c7edda3d91d62e
Jon Zingale <jonzingale@gmail.com>
2020-03-23 21:24:15 -0600

    towards MRecords

--- dynamical-monads/CsvRecord.hs
+++ dynamical-monads/CsvRecord.hs
@@ -10,16 +10,19 @@ import Data.Csv
 
 -- CSV Record
 {-- TODO: work out the details for join_src and join_tgt --}
-data DynamicalRecord s = DRecord { 
+data M_Record s = M1 {
   src :: s, tgt :: s,
   unit_src :: s, unit_tgt :: s -- values which map to src/tgt
   -- join_src :: [s], join_tgt :: [s] -- all values which collapse to src/tgt
 } deriving (Eq, Generic)
 
-instance ToNamedRecord (DynamicalRecord String)
+instance ToNamedRecord (M_Record String)
 
-instance DefaultOrdered (DynamicalRecord String) where
+instance DefaultOrdered (M_Record String) where
     headerOrder _ = header ["src", "tgt", "unit src", "unit tgt"]
+
+-- M_RECORD helpers
+
 
 -- Edge Record
 instance ToNamedRecord (Edge String)
--- dynamical-monads/Main.hs
+++ dynamical-monads/Main.hs
@@ -9,7 +9,7 @@ import Data.Text (Text)
 import Data.Csv
 
 import DynamicalExample (g1, g2, g3, g4)
-import CsvRecord (DynamicalRecord)
+import CsvRecord (M_Record)
 import Dynamical
 import Product
 
@@ -22,13 +22,17 @@ TODO:
 -- dynamicalCsv = do
 --   let g = g1 + g4
 --   let mg = eval.unit $ g
---   let mmg = eval.unit $ mg
---   let recs = [DynamicalRecord (E gs gt) ]
+--   -- let mmg = eval.unit $ mg
+--   let recs = [M1 s1 t1 s2 t2 | ((E s1 t1), E st2 t2) <- zip g (???)]
 --   BL.writeFile "./dynamical_record.csv"
+
+-- Perhaps instead of the above make a M1_Record which includes the unit name
+-- and make an M2_record which includes both unit name and mu name.
 
 main = do
   -- let g = g1 + g2 -- edge^2, 3-cycle with leg
-  let g = g1 + g4 -- 2-cycle, edge^2
+  -- let g = g1 + g4 -- 2-cycle, edge^2
+  let g = [E "0" "0", E "1" "0", E "2" "1"]
   let a = encodeDefaultOrderedByName g
   let m_a = encodeDefaultOrderedByName.eval.unit $ g
   let m_m_a = encodeDefaultOrderedByName $ (eval.unit).(eval.unit) $ g
