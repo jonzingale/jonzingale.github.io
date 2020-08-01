30253540967739f27453044498b5ba4fa60996ea
Jon Zingale <jonzingale@gmail.com>
2020-08-01 16:31:06 -0600

    lastFive days

--- covid19/helpers.hs
+++ covid19/helpers.hs
@@ -3,6 +3,7 @@ module Helpers where
 cases :: [Int]
 cases = [322,328,337,341,352,380,389,396,407,417,433,436,456,462,474,482,497,510,524,535,550,561]
 
+main = do avg cases
 
 diffs (c:cs) = zipWith (-) cs (c:cs)
 
@@ -12,4 +13,4 @@ avg cs = do
   let tot = foldr (+) 0 ds
   print $ div tot (length ds)
 
-main = do avg cases
+lastFive = avg $ drop (length cases - 5) cases
